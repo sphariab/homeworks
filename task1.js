98014a9ea3c5d3c6c8c974c616495247ff050375
ysharipbaeva <y.sharipbaeva@globit.com>
2022-11-20 13:56:25 +0200

    added isArray checks

--- task1.js
+++ task1.js
@@ -1,4 +1,10 @@
 const text = 'What is the average airspeed velocity of an unladen swallow';
 
-const getTheLongestWord = word => word && word.split(' ').reduce(
-    (accumulator, currentValue) => accumulator.length > currentValue.length ? accumulator : currentValue);
+const getTheLongestWord = word => {
+	if (typeof word !== 'string') return;
+
+	return word && word.split(' ').reduce(
+		(accumulator, currentValue) => accumulator.length > currentValue.length ? accumulator : currentValue);
+}
+
+console.log(getTheLongestWord(text))
--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
 const numbers = ['Name1', 'Name2', 'Name1', 'Name3', 'orange', 'blue', 'orange'];
 
-const removeDuplicates = values => values && values.reduce(
+const removeDuplicates = values => values && Array.isArray(values). values.reduce(
     (acc, currentValue) => acc.includes(currentValue) ? acc : [...acc, currentValue], []);
