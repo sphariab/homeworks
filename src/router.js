30c44462dd103103e8f3cd19c6bafd3e5ce70fcd
ysharipbaeva <sharipbaeva92@gmail.com>
2023-02-10 23:13:19 +0100

    gh-pages

--- src/router.js
+++ src/router.js
@@ -6,41 +6,39 @@ import AlbumsList from "./components/Albums/AlbumsList";
 import PhotosList from "./components/PhotosList";
 import Form from "./components/Users/Form";
 
-console.log(process.env.PUBLIC_URL)
-
 export const router = createBrowserRouter([
   {
-    path: `${process.env.PUBLIC_URL}/`,
+    path: `/`,
     element: <App />,
     children: [
       {
-        path: `${process.env.PUBLIC_URL}/users`,
+        path: `/users`,
         element: <UsersList />,
         children: [
           {
-            path: `${process.env.PUBLIC_URL}/users/:id/edit`,
+            path: `/users/:id/edit`,
             element: <Form />,
           },
           {
-            path: `${process.env.PUBLIC_URL}/users/add`,
+            path: `/users/add`,
             element: <Form />,
           }
         ]
       },
       {
-        path: `${process.env.PUBLIC_URL}/users/:id`,
+        path: `/users/:id`,
         element: <UserDetail />,
       },
       {
-        path: `${process.env.PUBLIC_URL}/users/:id/albums`,
+        path: `/users/:id/albums`,
         element: <AlbumsList />,
       },
       {
-        path: `${process.env.PUBLIC_URL}/albums`,
+        path: `/albums`,
         element: <AlbumsList />,
       },
       {
-        path: `${process.env.PUBLIC_URL}/albums/:id/photos`,
+        path: `/albums/:id/photos`,
         element: <PhotosList />,
       }
     ]
