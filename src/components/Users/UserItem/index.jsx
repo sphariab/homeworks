a5e6676d0489293f4334605fa0391837b33b0d0c
ysharipbaeva <sharipbaeva92@gmail.com>
2023-02-12 18:26:25 +0100

    icon open

--- src/components/Users/UserItem/index.jsx
+++ src/components/Users/UserItem/index.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import { NavLink, Link } from "react-router-dom";
 import Button from '../../UI/Button';
 import AlbumIcon from "../../../icons/AlbumIcon";
+import AlbumOpenIcon from "../../../icons/AlbumOpenIcon";
 import './styles.scss'
 
 
-const UserItem = ({ item , deleteUser }) => (
-	<>
-		<li className='user__name'>
-			<div className='user__name-container'>
-				<NavLink to='/users/${item.id}/albums' className='m-r'>
-					<AlbumIcon />
-				</NavLink>
-				<NavLink to={`/users/${item.id}`} >
-					{item.name}
-				</NavLink>
-			</div>
-			<div className='user__controls'>
-				<Link to={`/users/${item.id}/edit`} state={{ item }} className='button button_blue m-r'>Edit</Link>
-				<Button text='Delete' onClick={() => deleteUser(item.id)} className='button button_red m-r' />
-			</div>
-		</li>
-	</>
-);
+const UserItem = ({ item , deleteUser }) => {
+	const [isActive, setActive] = useState(false);
+	return (
+		<>
+			<li className='user__name'>
+				<div className='user__name-container'>
+					<NavLink to={`/users/${item.id}/albums`}
+									 className='m-r link'
+									 onMouseEnter={() => setActive(true)}
+									 onMouseLeave={() => setActive(false)}
+					>
+						{isActive ? <AlbumOpenIcon /> : <AlbumIcon />}
+					</NavLink>
+					<NavLink to={`/users/${item.id}`} >
+						{item.name}
+					</NavLink>
+				</div>
+				<div className='user__controls'>
+					<Link to={`/users/${item.id}/edit`} state={{ item }} className='button button_blue m-r'>Edit</Link>
+					<Button text='Delete' onClick={() => deleteUser(item.id)} className='button button_red m-r' />
+				</div>
+			</li>
+		</>
+	);
+}
 
 UserItem.propTypes = {
 	deleteUser: PropTypes.func.isRequired,
