7b763447d77550b0bfc556f1774f11ef4626f940
ysharipbaeva <sharipbaeva92@gmail.com>
2023-03-11 13:50:19 +0100

    get bonus check

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-var validPromoCodes = { 12345678: 10, 34562346: 1000, 12553523: 2000 };
-var getLength = function (number) {
-    var counter = 1;
-    while (number > 10) {
-        number = number / 10;
-        counter++;
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
     }
-    return counter;
+    return to.concat(ar || Array.prototype.slice.call(from));
 };
-var isValid = function (promo) { return getLength(promo) === 8 && validPromoCodes.hasOwnProperty(promo); };
-var getBonus = function (promo) { return isValid(promo)
-    ? Object.keys(validPromoCodes).length && validPromoCodes[promo] : 0; };
-console.log(getBonus(34562346));
+var isOdd = function (number) { return number % 2 > 0; };
+var isEven = function (number) { return number % 2 === 0; };
+var isOddPair = function (a, b) { return isOdd(a) && isOdd(b); };
+var isPalindrome = function (a, b) { return a === b; };
+var getSum = function (promo, helper) {
+    return toArray(promo).reduce(function (total, item) { return helper(item) ? item + total : total; }, 0);
+};
+var toArray = function (promo) {
+    var promoDigits = [];
+    while (promo > 0) {
+        promoDigits = __spreadArray(__spreadArray([], promoDigits, true), [Math.floor(promo % 10)], false);
+        promo = Math.floor(promo / 10);
+    }
+    return promoDigits.reverse();
+};
+var isEvenSumGreaterOddSum = function (promo) { return getSum(promo, isEven) > getSum(promo, isOdd); };
+var findOddDigits = function (promo) {
+    var digits = toArray(promo);
+    var odds = [];
+    for (var i = 0; i < digits.length; i++) {
+        var firstDigit = digits[i], secondDigit = digits[i + 1], thirdDigit = digits[i + 2];
+        if (isOddPair(firstDigit, secondDigit) && isEven(thirdDigit) && !isPalindrome(firstDigit, secondDigit)) {
+            odds = __spreadArray(__spreadArray([], odds, true), [firstDigit, secondDigit], false);
+        }
+        else if (odds.length && isOddPair(firstDigit, secondDigit) && !isPalindrome(firstDigit, secondDigit)) {
+            odds = __spreadArray(__spreadArray([], odds, true), [firstDigit, secondDigit], false);
+        }
+    }
+    return odds;
+};
+var checkIfAscOrderInOdds = function (promo) {
+    var digits = findOddDigits(promo);
+    var ascOdds = [];
+    for (var i = 0; i < digits.length; i = i + 2) {
+        var firstDigit = digits[i], secondDigit = digits[i + 1];
+        if (i % 2 === 0 && (firstDigit < secondDigit)) {
+            ascOdds = __spreadArray(__spreadArray([], ascOdds, true), [firstDigit, secondDigit], false);
+        }
+    }
+    return ascOdds.length === 4;
+};
+var getBonus = function (promo) {
+    if (toArray(promo).length === 8) {
+        if (checkIfAscOrderInOdds(promo)) {
+            return 2000;
+        }
+        if (findOddDigits(promo).length === 4) {
+            return 1000;
+        }
+        if (isEvenSumGreaterOddSum(promo)) {
+            return 100;
+        }
+        return 0;
+    }
+    return 0;
+};
--- index.ts
+++ index.ts
@@ -1,17 +1,73 @@
-type Promos = { [key: number]: number };
 type Promo = 100 | 1000 | 2000 | 0;
+type Helper = (promo: number) => boolean;
 
-const validPromoCodes: Promos = { 12345678: 10, 34562346: 1000, 12553523: 2000 };
+const isOdd = (number: number): boolean => number % 2 > 0;
+const isEven = (number: number): boolean => number % 2 === 0;
+const isOddPair = (a: number, b: number): boolean => isOdd(a) && isOdd(b);
+const isPalindrome = (a: number, b: number): boolean => a === b;
+const getSum = (promo: number, helper: Helper) =>
+  toArray(promo).reduce((total, item) => helper(item) ? item + total : total, 0)
 
-const getLength = (number: number): number => {
-    let counter: number = 1;
-    while (number > 10) {
-        number = number / 10;
-        counter++;
+const toArray = (promo: number): number[] => {
+    let promoDigits = [];
+    while (promo > 0) {
+        promoDigits = [...promoDigits, Math.floor(promo % 10)];
+        promo = Math.floor(promo / 10);
     }
 
-    return counter
+    return promoDigits.reverse();
 };
-const isValid = (promo: number): boolean => getLength(promo) === 8 && validPromoCodes.hasOwnProperty(promo);
-const getBonus = (promo: number): Promo => isValid(promo)
-  ? Object.keys(validPromoCodes).length && validPromoCodes[promo] as Promo : 0;
+
+const isEvenSumGreaterOddSum = (promo: number): boolean => getSum(promo, isEven) > getSum(promo, isOdd);
+
+const findOddDigits = (promo: number): number[] =>  {
+    const digits = toArray(promo);
+    let odds = [];
+
+    for (let i = 0; i < digits.length; i++) {
+        const firstDigit = digits[i], secondDigit = digits[i + 1], thirdDigit = digits[i + 2];
+
+        if (isOddPair(firstDigit, secondDigit) && isEven(thirdDigit) && !isPalindrome(firstDigit, secondDigit)){
+            odds = [...odds, firstDigit, secondDigit];
+        } else if(odds.length && isOddPair(firstDigit, secondDigit) && !isPalindrome(firstDigit, secondDigit)) {
+            odds = [...odds, firstDigit, secondDigit];
+        }
+    }
+
+    return odds;
+};
+
+const checkIfAscOrderInOdds = (promo: number): boolean => {
+    const digits = findOddDigits(promo);
+    let ascOdds = [];
+
+    for (let i = 0; i < digits.length; i = i + 2) {
+        const firstDigit = digits[i], secondDigit = digits[i + 1];
+
+        if (i % 2 === 0 && (firstDigit < secondDigit)) {
+            ascOdds = [...ascOdds, firstDigit, secondDigit]
+        }
+    }
+
+    return ascOdds.length === 4;
+};
+
+const getBonus = (promo: number): Promo => {
+    if (toArray(promo).length === 8) {
+        if (checkIfAscOrderInOdds(promo)) {
+            return 2000;
+        }
+
+        if (findOddDigits(promo).length === 4) {
+            return 1000;
+        }
+
+        if (isEvenSumGreaterOddSum(promo)) {
+            return 100;
+        }
+
+        return 0;
+    }
+
+    return 0;
+};
